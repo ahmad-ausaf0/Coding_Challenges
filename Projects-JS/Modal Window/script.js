e6826f1af71bc037cb6e0af13bb68e5613a678a3
ahmad-ausaf0 <ahmadausaf0@gmail.com>
2022-06-13 19:26:17 +0530

    Refactor Project code

--- Projects-JS/Modal Window/script.js
+++ Projects-JS/Modal Window/script.js
@@ -21,5 +21,10 @@ const closeModal = () => {
 };
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
